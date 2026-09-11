a581c95cf7e11c3c5314019d670dfb8501c539a6
senoldcell509-sudo <senoldcell509@gmail.com>
2026-09-11 03:05:18 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='tikane-mwen-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
